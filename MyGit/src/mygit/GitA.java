78962d1e799708d1e90525d7fbfdd45bec2c5360
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2023-12-21 17:12:49 +0900

    Modify A in master

--- MyGit/src/mygit/GitA.java
+++ MyGit/src/mygit/GitA.java
@@ -4,5 +4,6 @@ public class GitA {
 	public static void main(String[] args) {
 		System.out.println("This is GitA!");
 		System.out.println("check for connecting");
+		System.out.println("Modify A in master");
 	}
 }
