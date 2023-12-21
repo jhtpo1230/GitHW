e7ac92deb99def8bb880aac5f1faca1f431bfb8b
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2023-12-21 17:45:52 +0900

    Generate devB, devC and Modify A in master

--- MyGit/src/mygit/GitA.java
+++ MyGit/src/mygit/GitA.java
@@ -5,6 +5,6 @@ public class GitA {
 		System.out.println("This is GitA!");
 		System.out.println("check for connecting");
 		System.out.println("Modify A in master");
-		System.out.println("Generate devB, devC and Modify A in master");
+		System.out.println("Generate devB, devC and Modify A in master"); // Generate devB, devC and Modify A in master
 	}
 }
