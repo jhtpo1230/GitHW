e54c033161373edbb10afc34ce967a4b3e327231
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2023-12-21 17:41:55 +0900

    Modify A,C in devC

--- MyGit/src/mygit/GitC.java
+++ MyGit/src/mygit/GitC.java
@@ -4,5 +4,6 @@ public class GitC {
 	public static void main(String[] args) {
 		System.out.println("This is GitC!");
 		System.out.println("generate devC");
+		System.out.println("Modify C in devC");
 	}
 }
